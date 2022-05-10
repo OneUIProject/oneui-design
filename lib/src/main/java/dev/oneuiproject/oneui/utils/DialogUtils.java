158ce3e703c285d9d4c59a5e7b898a6d34f68a07
BlackMesa123 <giangrecosalvo9@gmail.com>
2022-05-10 15:30:50 +0200

    lib: utils: introduce DialogUtils
    Co-authored-by: Yann <yanndroid.dev@gmail.com>
    Signed-off-by: BlackMesa123 <giangrecosalvo9@gmail.com>

---
+++ lib/src/main/java/dev/oneuiproject/oneui/utils/DialogUtils.java
@@ -0,0 +1,80 @@
+package dev.oneuiproject.oneui.utils;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+
+import dev.oneuiproject.oneui.R;
+
+public class DialogUtils {
+    private static final String TAG = "DialogUtils";
+
+    public interface DialogProgressCallback {
+        void onClick(View v);
+    }
+
+    public static void setDialogProgressForButton(@NonNull AlertDialog dialog, int whichButton,
+                                         @Nullable DialogProgressCallback callback) {
+        if (dialog != null) {
+            Button dialogBtn = dialog.getButton(whichButton);
+            if (dialogBtn == null) {
+                Log.e(TAG, "setDialogProgressForButton: dialog button is null, " +
+                        "Ensure you're calling this after dialog.show()");
+                return;
+            }
+
+            dialogBtn.setOnClickListener(v -> {
+                replaceDialogBtnWithProgress(dialog, dialogBtn);
+
+                if (callback != null) {
+                    callback.onClick(v);
+                }
+            });
+        } else {
+            Log.e(TAG, "setDialogProgressForButton: dialog is null");
+        }
+    }
+
+    private static void replaceDialogBtnWithProgress(@NonNull AlertDialog dialog,
+                                                     @NonNull Button dialogBtn) {
+        Context context = dialog.getContext();
+
+        Button positiveBtn = dialog.getButton(DialogInterface.BUTTON_POSITIVE);
+        Button negativeBtn = dialog.getButton(DialogInterface.BUTTON_NEGATIVE);
+        Button neutralBtn = dialog.getButton(DialogInterface.BUTTON_NEUTRAL);
+
+        dialog.setCancelable(false);
+        dialog.setCanceledOnTouchOutside(false);
+        if (positiveBtn != null) positiveBtn.setEnabled(false);
+        if (negativeBtn != null) negativeBtn.setEnabled(false);
+        if (neutralBtn != null) neutralBtn.setEnabled(false);
+
+        ViewGroup buttonBar = (ViewGroup) dialogBtn.getParent();
+        if (buttonBar != null) {
+            final int btnIndex = buttonBar.indexOfChild(dialogBtn);
+
+            ViewGroup.LayoutParams lp = dialogBtn.getLayoutParams();
+            lp.width = context.getResources()
+                    .getDimensionPixelSize(R.dimen.sesl_dialog_button_min_height);
+            lp.height = context.getResources()
+                    .getDimensionPixelSize(R.dimen.sesl_dialog_button_min_height);
+
+            LayoutInflater inflater = LayoutInflater.from(context);
+            View progressView = inflater.inflate(
+                    R.layout.oui_view_dialog_progress_bar, buttonBar, false);
+            progressView.setLayoutParams(lp);
+
+            buttonBar.removeView(dialogBtn);
+            buttonBar.addView(progressView, btnIndex);
+        }
+    }
+
+}
