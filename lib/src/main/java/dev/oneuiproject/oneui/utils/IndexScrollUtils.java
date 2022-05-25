2a891a22fd0f664bf1909c3a45e2de54791a1d65
BlackMesa123 <giangrecosalvo9@gmail.com>
2022-05-25 13:20:39 +0200

    lib: utils: introduce IndexScrollUtils
    Signed-off-by: BlackMesa123 <giangrecosalvo9@gmail.com>

---
+++ lib/src/main/java/dev/oneuiproject/oneui/utils/IndexScrollUtils.java
@@ -0,0 +1,46 @@
+package dev.oneuiproject.oneui.utils;
+
+import android.util.Log;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+
+import androidx.annotation.NonNull;
+import androidx.indexscroll.widget.SeslIndexScrollView;
+
+import dev.oneuiproject.oneui.R;
+
+public class IndexScrollUtils {
+    private static final String TAG = "IndexScrollUtils";
+
+    public static void animateVisibility(@NonNull SeslIndexScrollView indexScroll,
+                                          boolean visible) {
+        if (indexScroll != null) {
+            boolean isViewVisible = indexScroll.getVisibility() == View.VISIBLE;
+
+            if (isViewVisible != visible) {
+                Animation anim = AnimationUtils.loadAnimation(indexScroll.getContext(), visible
+                        ? R.anim.oui_index_scroll_show
+                        : R.anim.oui_index_scroll_hide);
+                anim.setAnimationListener(new Animation.AnimationListener() {
+                    @Override
+                    public void onAnimationStart(Animation animation) { }
+
+                    @Override
+                    public void onAnimationEnd(Animation animation) {
+                        indexScroll.setVisibility(visible
+                                ? View.VISIBLE
+                                : View.GONE);
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animation animation) { }
+                });
+
+                indexScroll.startAnimation(anim);
+            }
+        } else {
+            Log.e(TAG, "animateVisibility: indexScroll is null");
+        }
+    }
+}
