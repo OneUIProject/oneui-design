d933c0b8e9f11f91430789ee2424336f258519d9
BlackMesa123 <giangrecosalvo9@gmail.com>
2022-07-30 10:15:16 +0200

    sample-app: WidgetsFragment: add Spinner/SearchView example, adjust buttons example
    Signed-off-by: BlackMesa123 <giangrecosalvo9@gmail.com>

--- sample-app/src/main/java/dev/oneuiproject/oneuiexample/fragment/WidgetsFragment.java
+++ sample-app/src/main/java/dev/oneuiproject/oneuiexample/fragment/WidgetsFragment.java
@@ -1,9 +1,57 @@
 package dev.oneuiproject.oneuiexample.fragment;
 
+import android.app.SearchManager;
+import android.content.ComponentName;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.ArrayAdapter;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.appcompat.widget.AppCompatSpinner;
+import androidx.appcompat.widget.SearchView;
+
 import com.sec.sesl.tester.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import dev.oneuiproject.oneuiexample.activity.MainActivity;
 import dev.oneuiproject.oneuiexample.base.BaseFragment;
 
-public class WidgetsFragment extends BaseFragment {
+public class WidgetsFragment extends BaseFragment
+        implements View.OnClickListener {
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        AppCompatButton btn_1 = view.findViewById(R.id.fragment_btn_1);
+        btn_1.setOnClickListener(this);
+        AppCompatButton btn_2 = view.findViewById(R.id.fragment_btn_2);
+        btn_2.setOnClickListener(this);
+        AppCompatButton btn_3 = view.findViewById(R.id.fragment_btn_3);
+        btn_3.setOnClickListener(this);
+
+        AppCompatSpinner spinner = view.findViewById(R.id.fragment_spinner);
+        List<String> items = new ArrayList<>();
+        for (int i = 1; i < 5; i++)
+            items.add("Spinner Item " + i);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(mContext,
+                android.R.layout.simple_spinner_item, items);
+        adapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        SearchView searchView = view.findViewById(R.id.fragment_searchview);
+        SearchManager manager = (SearchManager) mContext.getSystemService(Context.SEARCH_SERVICE);
+        searchView.setSearchableInfo(manager.getSearchableInfo(
+                new ComponentName(mContext, MainActivity.class)));
+        searchView.seslSetUpButtonVisibility(View.VISIBLE);
+        searchView.seslSetOnUpButtonClickListener(this);
+    }
 
     @Override
     public int getLayoutResId() {
@@ -18,6 +66,11 @@ public class WidgetsFragment extends BaseFragment {
     @Override
     public CharSequence getTitle() {
         return "Widgets";
+    }
+
+    @Override
+    public void onClick(View v) {
+        // no-op
     }
 
 }
